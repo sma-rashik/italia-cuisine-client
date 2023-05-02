d31e30ed89c8ffb58261bdb74942ed71d0386516
Rashik <smarashik00@gmail.com>
2023-05-02 21:49:30 +0600

    chef data added

--- src/component/chef/Chef.jsx
+++ src/component/chef/Chef.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import { FaCheck, FaThumbsUp } from "react-icons/fa";
 
 const Chef = ({ chef }) => {
-  const { name, picture, id } = chef;
+  const { name, picture, id, experience_years, num_recipes, likes } = chef;
   return (
     <div>
       <div className="mt-10 card card-compact w-96 bg-base-100 shadow-xl">
         <figure>
           <img className="h-64 w-64 " src={picture} alt="Chef" />
         </figure>
-        <div className="card-body">
-          <div className="text-center">
-            <h2 className="card-title ">{name}</h2>
-          </div>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">View Chef</button>
+        <div className="card-body ">
+          <h2 className="text-center text-3xl text-indigo-900">{name}</h2>
+          <div className="ml-15">
+            <p className="flex text-lg gap-2">
+              <FaCheck className="mt-1" /> Experience: {experience_years} Years
+            </p>
+            <p className="flex text-lg gap-2">
+              <FaCheck className="mt-1" /> Number of Recipes: {num_recipes}
+            </p>
+            <p className="flex text-lg gap-2">
+              <FaThumbsUp className="mt-1" /> Total Likes: {likes}
+            </p>
           </div>
         </div>
       </div>
@@ -22,8 +29,3 @@ const Chef = ({ chef }) => {
 };
 
 export default Chef;
-{
-  /* {chefs.map((chef) => (
-        <Chef key={chef.id} chef={chef && chef.id ? chef : {}}></Chef>
-      ))} */
-}
